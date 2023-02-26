8716fb1e430a8f7f17287075135fe00739ed8109
19Edwin92 <b_a_s_e@naver.com>
2023-02-27 00:27:59 +0900

    ver1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+import Header from './componenets/Header'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// styled-components 설치하기
+// 말그대로 스타일된 컴포넌트를 제작할 수 있는 리액트 라이브러리이다. 
+// 설치해보자. yarn add styled-components
+// createGlobalStyle 은 라이브러리에서 제공하는 기본설정으로 사용하기 위해서는 상단에 임포트해야 한다. 
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Dovemayo_gothic';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2302@1.1/Dovemayo_gothic.woff2') format('woff2');
+  font-weight: normal;
+  font-style: normal;
 }
 
-export default App;
+  body {
+    background: #e9ecef;
+  }
+`
+const LayOut = styled.div`
+  max-width: 1200px;
+  min-whith: 800px;
+  margin: 0 auto;
+`
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+      <LayOut>
+        <Header></Header>
+      </LayOut>
+    </>
+  )
+}
+
+export default App
---
+++ src/componenets/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Headers = styled.header`
+  font-family: 'Dovemayo_gothic';
+  height: 50px;
+  line-height: 50px;
+  display: grid ;
+  grid-template-columns: 1fr 1fr 1fr;
+  font-size: 25px;
+  background-color: white;
+  border-bottom: 3px solid black;
+  box-shadow: 0px 4px 3px rgba(0, 0, 0, 0.3);
+
+  div:nth-child(1) {
+    margin-left: 10px;
+
+    span:nth-child(1) {
+      font-weight: 800;
+    }
+    span:nth-child(2) {
+      font-size:20px
+    }
+  }
+
+  div:nth-child(3) {
+    margin-right: 10px;
+    text-align:end
+    
+  }
+`
+
+
+function Header() {
+  return (
+    <Headers>
+      <div><span>TODO</span><span> -lists</span></div>
+      <div></div>
+      <div>Edwin</div>
+    </Headers>
+  )
+}
+
+export default Header
